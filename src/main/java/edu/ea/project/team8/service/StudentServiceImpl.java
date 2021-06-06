2457c2ff667fbf60b72e4efbe601dedf6c2c9657
Kasim <se.kasim.ebrahim@gmail.com>
2021-06-06 05:27:04 -0500

    Get courses taking now and already taken
    squash

--- src/main/java/edu/ea/project/team8/service/StudentServiceImpl.java
+++ src/main/java/edu/ea/project/team8/service/StudentServiceImpl.java
@@ -1,7 +1,11 @@
 package edu.ea.project.team8.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import edu.ea.project.team8.domain.Course;
+import edu.ea.project.team8.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,11 +16,27 @@ import edu.ea.project.team8.domain.Student;
 @Service("studentService")
 @Transactional(propagation=Propagation.REQUIRED)
 public class StudentServiceImpl implements StudentService {
+	private StudentRepository repository;
+
+	@Autowired
+	public StudentServiceImpl(StudentRepository repository) {
+		this.repository = repository;
+	}
 
 	@Override
 	public List<Student> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Course> findAllCoursesTaking(Integer id) {
+		return repository.findAllCoursesTaking(id, LocalDate.now());
+	}
+
+	@Override
+	public List<Course> findAllCoursesTaken(Integer id) {
+		return repository.findAllCoursesTakenBefore(id, LocalDate.now());
 	}
 
 }
