4fd1f12e5e2a17ed18e917253405d7053c1f5e22
JaCkIsO <jackisodev@gmail.com>
2021-06-08 18:27:15 -0500

    Update CourseController.java
    Add BaseController

--- src/main/java/edu/ea/project/team8/controller/CourseController.java
+++ src/main/java/edu/ea/project/team8/controller/CourseController.java
@@ -1,4 +1,11 @@
 package edu.ea.project.team8.controller;
 
-public class CourseController {
+import edu.ea.project.team8.common.controller.BaseController;
+import edu.ea.project.team8.domain.Course;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController extends BaseController<Course, Integer> {
 }
