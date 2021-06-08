0ce6608def6f068c3d45551e22dcd727f5fe80aa
JaCkIsO <jackisodev@gmail.com>
2021-06-08 02:40:02 -0500

    Extend and Implement UserDetailsService

--- src/main/java/edu/ea/project/team8/service/UserService.java
+++ src/main/java/edu/ea/project/team8/service/UserService.java
@@ -1,10 +1,11 @@
 package edu.ea.project.team8.service;
 
 import edu.ea.project.team8.domain.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 	public List<User> findAll();
 	public void addUser(User user);
 }
--- src/main/java/edu/ea/project/team8/service/UserServiceImpl.java
+++ src/main/java/edu/ea/project/team8/service/UserServiceImpl.java
@@ -1,13 +1,19 @@
 package edu.ea.project.team8.service;
 
+import edu.ea.project.team8.config.security.CustomUserDetails;
 import edu.ea.project.team8.domain.User;
 import edu.ea.project.team8.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service("userService")
 @Transactional(propagation= Propagation.REQUIRED)
@@ -26,6 +32,13 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public void addUser(User user) {
+		user.setPasswordHash(new BCryptPasswordEncoder().encode(user.getPasswordHash()));
 		repository.save(user);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<User> user = repository.findByUsername(username);
+		return new CustomUserDetails(user.orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username)));
 	}
 }
