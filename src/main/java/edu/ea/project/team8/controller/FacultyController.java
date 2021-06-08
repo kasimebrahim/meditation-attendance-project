626a63582072ca1150145760bdbc9d332b9d9d17
JaCkIsO <jackisodev@gmail.com>
2021-06-08 18:35:28 -0500

    Add CrossOrigin

--- src/main/java/edu/ea/project/team8/controller/FacultyController.java
+++ src/main/java/edu/ea/project/team8/controller/FacultyController.java
@@ -2,10 +2,12 @@ package edu.ea.project.team8.controller;
 
 import edu.ea.project.team8.common.controller.BaseController;
 import edu.ea.project.team8.domain.Faculty;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/faculties")
+@CrossOrigin
 public class FacultyController extends BaseController<Faculty, Integer> {
 }
--- src/main/java/edu/ea/project/team8/controller/RoleController.java
+++ src/main/java/edu/ea/project/team8/controller/RoleController.java
@@ -2,10 +2,12 @@ package edu.ea.project.team8.controller;
 
 import edu.ea.project.team8.common.controller.BaseController;
 import edu.ea.project.team8.domain.Role;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/roles")
+@CrossOrigin
 public class RoleController extends BaseController<Role, Integer> {
 }
