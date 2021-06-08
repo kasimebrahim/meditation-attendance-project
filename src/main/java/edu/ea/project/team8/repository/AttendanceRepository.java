22a7f6fdc0960639c444774bf9443612314e6af2
Kasim <se.kasim.ebrahim@gmail.com>
2021-06-08 14:12:58 -0500

    Implement basic attendance queries

--- src/main/java/edu/ea/project/team8/repository/AttendanceRepository.java
+++ src/main/java/edu/ea/project/team8/repository/AttendanceRepository.java
@@ -2,6 +2,8 @@ package edu.ea.project.team8.repository;
 
 import edu.ea.project.team8.domain.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,9 +14,12 @@ import java.util.List;
 @Transactional(propagation = Propagation.MANDATORY)
 public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {
 
-	List<Attendance> findByStudentId(Integer personId);
+	@Query("FROM Attendance ac WHERE ac.student.id = :sid")
+	List<Attendance> findByStudentId(@Param("sid") Integer sid);
 
-	List<Attendance> findByCourse(Integer courseId);
+	@Query("SELECT ac FROM Attendance ac WHERE ac.classSession.offering.course.id = :cid")
+	List<Attendance> findByCourse(@Param("cid") Integer cid);
 
-	List<Attendance> findOfByCourse(Integer personId, Integer courseId);
+	@Query("SELECT ac FROM Attendance ac WHERE ac.student.id = :sid and ac.classSession.offering.course.id = :cid")
+	List<Attendance> findOfByCourse(@Param("sid") Integer sid, @Param("cid") Integer cid);
 }
