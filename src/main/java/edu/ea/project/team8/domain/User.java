c87bad8d224af36c54d6c73da86d6a116cbef5b1
JaCkIsO <jackisodev@gmail.com>
2021-06-08 02:37:19 -0500

    Update User.java
    - Change Collection of Role to Set avoid duplicate roles

--- src/main/java/edu/ea/project/team8/domain/User.java
+++ src/main/java/edu/ea/project/team8/domain/User.java
@@ -2,52 +2,51 @@ package edu.ea.project.team8.domain;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
-import java.util.Collection;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "EA_User")
 public class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-	private String name;
+    private String username;
 
-	@Column(name = "password_hash")
-	private String passwordHash;
+    @Column(name = "password_hash")
+    private String passwordHash;
 
-	/*
-	* Note: This is a uni-directional relationship.
-	*       what ever information <Person> needs from <User>
-	*       it should be available in the token.
-	* */
-	@OneToOne
-	@JoinColumn(name = "person_id", nullable = true)
-	private Person person;
+    /*
+     * Note: This is a uni-directional relationship.
+     *       what ever information <Person> needs from <User>
+     *       it should be available in the token.
+     * */
+    @OneToOne
+    @JoinColumn(name = "person_id", nullable = true)
+    private Person person;
 
-	@ManyToMany
-	@JoinTable(name = "User_Role",
-			joinColumns = {@JoinColumn(name = "user_id")},
-			inverseJoinColumns = {@JoinColumn(name = "role_id")}
-	)
-	private Collection<Role> roles;
+    @ManyToMany
+    @JoinTable(name = "User_Role",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")}
+    )
+    private Set<Role> roles = new HashSet<>();
 
-	public User(String name, String passwordHash) {
-		this.name = name;
-		this.passwordHash = passwordHash;
-	}
+    public User(String username, String passwordHash) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
 
-	public void setPerson(Person person) {
-		this.person = person;
-	}
+    public void setPerson(Person person) {
+        this.person = person;
+    }
 
-	public void addRole(Role role) {
-		this.roles.add(role);
-	}
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
 }
