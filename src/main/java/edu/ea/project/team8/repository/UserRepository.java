e2e89d26162104f20db3b5a32361802ff9d8d142
JaCkIsO <jackisodev@gmail.com>
2021-06-08 02:38:56 -0500

    Add loadByUsername

--- src/main/java/edu/ea/project/team8/repository/UserRepository.java
+++ src/main/java/edu/ea/project/team8/repository/UserRepository.java
@@ -6,7 +6,10 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Repository
 @Transactional(propagation = Propagation.MANDATORY)
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }
