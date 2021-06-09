a0bb95c528d0833238ddf08629fb4d3011e0c7b2
JaCkIsO <jackisodev@gmail.com>
2021-06-09 14:27:54 -0500

    Fix dup url

--- src/main/java/edu/ea/project/team8/controller/BarcodeRecordController.java
+++ src/main/java/edu/ea/project/team8/controller/BarcodeRecordController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/persons")
+@RequestMapping("/barcodes")
 @CrossOrigin(origins = "*")
 public class BarcodeRecordController extends BaseController<BarCodeRecord, BarCodeRecord, Integer> {
 }
